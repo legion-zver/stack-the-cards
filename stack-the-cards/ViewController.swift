64430fe8a5384143afc3407e8e3dd5ddbeaa87e4
Alexander Garin <garin1221w@gmail.com>
2016-10-22 18:34:21 +0500

    Initial Commit

---
+++ stack-the-cards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stack-the-cards
+//
+//  Created by Александр Зверь on 22.10.16.
+//  Copyright © 2016 ALEXANDER GARIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
